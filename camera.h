7d209257e23d773fd1cae6ac4a36dff64d58d3be
Hamiz <ultimatehamiz@gmail.com>
2025-05-15 14:31:05 -0500

    added positionable camera

--- camera.h
+++ camera.h
@@ -12,8 +12,13 @@ class camera {
         double aspect_ratio = 1.0;
         int image_width = 100;
         int samples_per_pixel = 10; // number of samples to pull per pixel for antialiasing
-        int max_depth = 10;
+        int max_depth = 10; //number of bounces for a ray
 
+        // more camera features
+        double vfov = 90; //vertical (fov) viewing angle
+        point3 lookfrom = point3(0,0,0); //camera center
+        point3 lookat = point3(0,0,-1); // point camera is looking at
+        vec3 vup = vec3(0,1,0); // camera relative up direction
 
         void render(const hittable& world) {
             initialize();
@@ -55,6 +60,8 @@ class camera {
         vec3 pixel_delta_v;
         double pixel_sample_scale; // color scale factor for sum of pixel samples (?)
 
+        vec3 u, v, w; // basis vectors for camera
+
 
         void initialize() {
             
@@ -65,25 +72,35 @@ class camera {
 
             pixel_sample_scale = 1.0 / samples_per_pixel;
 
+            center = lookfrom;
+
 
             // camera and viewport stuff
             // try this with a different viewport height, it shouldn't matter -> scales stuff out to same value
-            auto viewport_height = 2.0;
+            // also fov stuff
+            auto focal_length = (lookfrom - lookat).length();
+            auto theta = degree_to_radians(vfov);
+            auto h = std::tan(theta/2);
+            auto viewport_height = 2.0 * h * focal_length;
             auto viewport_width = viewport_height * (double(image_width)/image_height); //isnt this just the aspect ratio? -> ideal ratio not necessacrily exact
-            auto focal_length = 1.0;
-            center = point3(0,0,0);
+            // center = point3(0,0,0);
+
+            // u,w,v basis vector
+            w = unit_vector(lookfrom - lookat); //-w is where camera is looking towards
+            u = unit_vector(cross(vup,w)); // width
+            v = cross(w,u); // height
 
 
             // horizontal and vertical vectors along viewport
-            auto viewport_u = vec3(viewport_width,0,0);
-            auto viewport_v = vec3(0, -viewport_height, 0);
+            auto viewport_u = viewport_width * u;
+            auto viewport_v = viewport_height * -v;
 
             // horizontal and vertical pixel deltas
             pixel_delta_u = viewport_u/ image_width;
             pixel_delta_v = viewport_v/image_height;
 
             // (offset) location of upper left pixel
-            auto viewport_upper_left = center - vec3(0,0,focal_length) - viewport_u/2 - viewport_v/2;
+            auto viewport_upper_left = center - (focal_length * w) - viewport_u/2 - viewport_v/2;
             pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v); 
 
         }
